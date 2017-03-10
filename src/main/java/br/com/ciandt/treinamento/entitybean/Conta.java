05fa6ee0d9eb73e48015bac8d876ac85da22a6fd
rodrigo-aversi <aversi83@gmail.com>
2017-03-10 09:21:58 -0300

    Primeiro Exemplo

--- src/main/java/br/com/ciandt/treinamento/entitybean/Conta.java
+++ src/main/java/br/com/ciandt/treinamento/entitybean/Conta.java
@@ -8,6 +8,9 @@ import javax.persistence.Id;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
+
 @Entity
 public class Conta {
 
@@ -15,28 +18,36 @@ public class Conta {
 	@GeneratedValue
 	private Long id;
 	private String titular;
-	private String agencia;
+	private Integer agencia;
 	private String numero;
 	private String banco;
 	@Temporal(TemporalType.TIMESTAMP)
 	private Date dataCriacao;
 	
+	private Long dataGravadaComoLong;
+	
 	public Conta() {}
 	
-	public Conta(String titular, String agencia, String numero, String banco, Date dataCriacao) {
+	public Conta(String titular, Integer agencia, String numero, String banco, Date dataCriacao, Long dataGravadaComoLong) {
 		super();
 		this.titular = titular;
 		this.agencia = agencia;
 		this.numero = numero;
 		this.banco = banco;
 		this.dataCriacao = dataCriacao;
+		this.dataGravadaComoLong = dataGravadaComoLong;
+	}
+	
+	public DateTime pegarDataLongComoDateTime() {
+		return new DateTime(this.dataGravadaComoLong, DateTimeZone.forID("America/Sao_Paulo"));
 	}
 
 	@Override
 	public String toString() {
-		return "Conta [id=" + id + ", titular=" + titular + ", agencia="
-				+ agencia + ", numero=" + numero + ", banco=" + banco
-				+ ", dataCriacao=" + dataCriacao + "]";
+		return "Conta [id=" + id + ", titular=" + titular + ", agencia=" + agencia + ", numero=" + numero + ", banco="
+				+ banco + ", dataCriacao=" + dataCriacao + ", dataGravadaComoLong=" + dataGravadaComoLong + "]";
 	}
+
+	
 }
 
--- src/test/java/br/com/ciandt/treinamento/entitybean/ContaTest.java
+++ src/test/java/br/com/ciandt/treinamento/entitybean/ContaTest.java
@@ -1,12 +1,13 @@
 package br.com.ciandt.treinamento.entitybean;
 
-import java.util.Calendar;
 import java.util.Date;
 
 import javax.persistence.EntityManager;
 import javax.persistence.Persistence;
 
 import org.apache.log4j.Logger;
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
 import org.junit.Test;
 
 public class ContaTest {
@@ -14,10 +15,12 @@ public class ContaTest {
 	final static Logger logger = Logger.getLogger(ContaTest.class);
 	
 	@Test
-	public void main() {
+	public void testMyFirst() {
 		
-		Date currentDate = new Date(Calendar.getInstance().getTimeInMillis());
-		Conta conta = new Conta("123","Bradesco", "123456-7","Rodrigo", currentDate);
+		Date currentDate = new Date();
+		DateTime dataAtualização = new DateTime(2017, 4, 1, 10, 0, DateTimeZone.forID("America/Sao_Paulo"));
+		
+		Conta conta = new Conta("FULANO DE TAL", 123, "123345678", "XPTO", currentDate, dataAtualização.getMillis());
 
 		EntityManager entityManager = Persistence.createEntityManagerFactory(
 				"tarefas").createEntityManager();
